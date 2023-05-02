c140af2868e5bd707ac92f82c92b12767780d5c7
Dario <dario.rubado@cimafoundation.org>
2023-05-02 12:38:07 +0200

    Added Dockerfile

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import telebot
+from telebot import custom_filters
 import os
 from dotenv import load_dotenv
 
@@ -9,13 +10,6 @@ load_dotenv()
 HTTP_API = os.getenv('HTTP_API')
 
 bot = telebot.TeleBot(HTTP_API)
-
-
-# Handle '/start' and '/help'
-#questa funzione viene eseguita quando si scrive /start
-# @bot.message_handler(commands=['help', 'start'])
-# def send_welcome(message):
-#     bot.send_voice(message.chat.id, open('audio.mp3', 'rb'))
 
 
 @bot.message_handler(commands=['qualecitta'])
@@ -93,11 +87,8 @@ def send_welcome(message):
     bot.send_voice(message.chat.id, open('audio/valutazionepeni.mpeg', 'rb'))
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-#questa funzione viene eseguita quando si scrive qualsiasi messaggio
-# @bot.message_handler(func=lambda message: True)
-# def echo_message(message):
-#     bot.reply_to(message, 'Ciaoooooooo')
+
+
 
 
 bot.polling()
